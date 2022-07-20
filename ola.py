20eb19a974f41b82c936a114c57d41aeaf2f912a
KellyyAnn <109698881+KellyyAnn@users.noreply.github.com>
2022-07-20 15:35:39 -0600

    Add files via upload

---
+++ ola.py
@@ -0,0 +1,6 @@
+import random
+ekis=["Abdiel", "Adrian", "Alexandra", "Carlos", "Diego", "Eileen", "Emily", "Fernando", "Hazel.O", "Machado", "Helen", "Himer", "Jaqueline", "Jennifer", "Charleen", "Josue", "Katherine", "Keiry", "Lili", "Linette", "Luis", "Manuel", "Nicole", "Oscar", "Saul", "Vladi", "Chris", "Santos", "Cristina", "Richard"]
+ye=["Papas", "Galleta de vainilla", "Galleta de chocolate", "Paleta de leche de unicornio", "Bon o Bon negro", "Bon o Bon blanco", "Bon o Bon rosado", "Viaje en la 29A", "Abrazo de miss Queen", "Saludo de Alex sensei", "Siga participando", "Nada :D", "Saludo de T. Fonseca",]
+var=random.choice(ekis)
+var2=random.choice(ye)
+print("El ganador es:", var, "Tu premio es:", var2)
